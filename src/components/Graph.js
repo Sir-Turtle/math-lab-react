a3523484cf8ec5f71979431d4a3798cacef32766
Sir-Turtle <tolgaakkaya@live.ca>
2017-12-04 22:31:26 -0500

    Refactored the Graph component to be a function component instead of a class since it doesn't have personalized event handlers or a state and is just presentational

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-class Graph extends React.Component {
-  render() {  
-    var graphContent = (
-      <LineChart width={this.props.width} height={this.props.height} data={this.props.graphData}>
+
+function Graph (props) {  
+    return (
+      <LineChart width={props.width} height={props.height} data={props.graphData}>
         <Line type="monotone" dataKey="yValue" stroke="#8884d8" />
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="xValue" />
         <YAxis dataKey="yValue"/>
       </LineChart>
     );
-    return (
-      <div >
-        {graphContent}
-      </div>
-    );
-  }
 };
 
 export default Graph;
